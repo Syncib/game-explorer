159303710ecc9e77ca68e94b17a2b9c9103008e4
Syncib <saqibabbas@live.co.uk>
2024-08-21 01:15:37 +0500

    Created a generic data fetching hook

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,42 +1,9 @@
-import { CanceledError } from "axios";
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-import { Game } from "./useGames";
-interface Genre {
-    id:number;
-    name:string;
+import useData from "./useData";
+export interface Genre {
+    id: number;
+    name: string;
 }
 
-interface FetchGenresResponse{
-    count:number;
-    results:Genre[];
-}
+const useGenres = () => useData<Genre>('/genres');
 
-const useGenres = () => {
-    const [genres, setGames] = useState<Genre[]>([]);
-    const [error, setError] = useState("");
-    const [isLoading, setLoading] = useState(false);
-    useEffect(() => {
-      const controller = new AbortController();
-      setLoading(true);
-      apiClient
-        .get<FetchGenresResponse>("/genres", { signal: controller.signal })
-        .then((res) => {
-          {
-            setGames(res.data.results);
-            setLoading(false);
-          }
-        })
-        .catch((err) => {
-          if (err instanceof CanceledError) return;
-          setError(err.message);
-          setLoading(false);
-        });
-      return () => {
-        controller.abort();
-      };
-    }, []);
-    return { genres, error, isLoading };
-}
-
-export default useGenres
+export default useGenres;
